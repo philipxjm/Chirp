0103ceb8e3471b4f0258be46511fe6bfa2cffa44
Tynan Couture-Rashid <tynancr@gmail.com>
2017-02-04 16:13:26 -0500

    Added serializers, urls, and views to main app.

---
+++ chirp/chirp/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from . import serializers
+from . import analyzer
+
+
+@api_view(['GET'])
+def analyzed_tweet_list(request):
+    serializer = serializers.AnalyzedTweetSerializer(analyzer.runSearchAnalysis(["trump"], count=1), many=True)
+    return Response(serializer.data)
+
+
