f5876d58c0b377d65bce75f8364e92d919c462b2
Tynan Couture-Rashid <tynancr@gmail.com>
2017-02-05 03:11:38 -0500

    Implemented querying and persistence for analyzed tweets.

--- chirp/tweetanalysis/views.py
+++ chirp/tweetanalysis/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
 from . import serializers
 from . import models
+from . import analyzer
 
-# Create your views here.
+
 @api_view(['GET'])
-def analyzed_tweet_list_trump(request):
-    tweets = models.AnalyzedTweetModel.objects.all()
-    serializer = serializers.AnalyzedTweetModelSerializer(tweets, many=True)
-    return Response(serializer.data)
+def analyzed_tweet_list(request, query):
+
+    tweets = models.AnalyzedTweetModel.objects.filter(search_string=query)
+
+    if(tweets):
+        serializer = serializers.AnalyzedTweetModelSerializer(tweets, many=True)
+        return Response(serializer.data)
+    else:
+        tweets = analyzer.runSearchAnalysis([query], count=400)
+
+        for tweet in tweets:
+            analyzed_tweet = models.AnalyzedTweetModel()
+            analyzed_tweet.search_string = query
+            analyzed_tweet.twitID = tweet.twitID
+            analyzed_tweet.label = tweet.label
+            analyzed_tweet.longitude = tweet.location[1]
+            analyzed_tweet.latitude = tweet.location[0]
+            analyzed_tweet.time = tweet.time
+            analyzed_tweet.prob_pos = tweet.prob_pos
+            analyzed_tweet.prob_neg = tweet.prob_neg
+            analyzed_tweet.prob_neu =tweet.prob_neu
+            analyzed_tweet.save()
+
+        tweets = models.AnalyzedTweetModel.objects.filter(search_string=query)
+        serializer = serializers.AnalyzedTweetModelSerializer(tweets, many=True)
+        return Response(serializer.data)
