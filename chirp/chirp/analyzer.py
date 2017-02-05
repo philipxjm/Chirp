178cb6b6ed1ea9cb0180d3872546abdc4a9cfb58
Philip Xu <philipxjm1@gmail.com>
2017-02-04 19:23:49 -0500

    used google geocoding library

--- chirp/chirp/analyzer.py
+++ chirp/chirp/analyzer.py
@@ -1,14 +1,8 @@
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-from TwitterSearch import *
 import json
-import pickle
-
+import requests
 from TwitterAPI import TwitterAPI
-api = TwitterAPI("sNjj2O9xgtclg2l4Y3batJNmD", "iKMk9pye8bBZLPzGBupCco2cEVKG8buESq4m2UUuaI5Br7c1RH", "2382398376-zmcPodEblLN3v3aiJ1uHoEAAJp2XJQ5lDO7xc5a", "17X7Dk2LrWY4BEUhsBsjtciSCGJXdslNSRqk4hmWfebhg")
-r = api.request('search/tweets', {'q':'pizza'})
-for item in r:
-        print(item)
 
 class AnalyzedTweet(object):
 	"""docstring for AnalyzedTweet"""
@@ -20,8 +14,18 @@ class AnalyzedTweet(object):
 		self.prob_neg = prob_neg
 		self.prob_neu = prob_neu
 
-	def toString(self):
+	def __str__(self):
 		return "{twitID: " + str(self.twitID) + ", location: " + str(self.location) + ", label: " + str(self.label) + ", prob_pos: " + str(self.prob_pos) + ", prob_neg: " + str(self.prob_neg) + ", prob_neu: " + str(self.prob_neu) + "}"
+
+def geocode(locStr):
+	api = "AIzaSyBZ68_Iz3U1Wpdo18viv0lV4X0PGO79jhg"
+	url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locStr + "&key= " + api
+	print(url)
+	r = requests.get(url)
+	if r.json()["status"] == "OK":
+		return [r.json()["results"][0]["geometry"]["location"]["lat"], r.json()["results"][0]["geometry"]["location"]["lng"]]
+	else:
+		return "BAD. DISCARD."
 
 def analyze(tweets):
 	analyzedTweets = []
@@ -36,27 +40,28 @@ def analyze(tweets):
 			result["probability"]["pos"], 
 			result["probability"]["neg"], 
 			result["probability"]["neutral"])
-		print(analyzedTweet.toString())
+		print(analyzedTweet)
 		analyzedTweets.append(analyzedTweet)
 	return analyzedTweets
 
-def search(keywords, count=100):
+def search(keyword, count=100):
 	tweets = []
 
 	api = TwitterAPI("sNjj2O9xgtclg2l4Y3batJNmD", "iKMk9pye8bBZLPzGBupCco2cEVKG8buESq4m2UUuaI5Br7c1RH", "2382398376-zmcPodEblLN3v3aiJ1uHoEAAJp2XJQ5lDO7xc5a", "17X7Dk2LrWY4BEUhsBsjtciSCGJXdslNSRqk4hmWfebhg")
-	r = api.request('search/tweets', {'q':'pizza'})
+	r = api.request('search/tweets', {'q':keyword})
 
 	for tweet in r:
 		# print(tweet)
-		# tweets.append(tweet)
-		tweets.append([tweet["id_str"], 
-			tweet["text"], 
-			tweet["user"]["location"]])
+		geo = geocode(tweet["user"]["location"])
+		if geo != "BAD. DISCARD.":
+			tweets.append([tweet["id_str"], 
+				tweet["text"], 
+				geo])
 
-	pickle.dump(tweets, open("tweets.p", "wb" ) )
 	return tweets
 
-def runSearchAnalysis(keywords, count=100):
-	return analyze(search(keywords, count))
+def runSearchAnalysis(keyword, count=100):
+	return analyze(search(keyword, count))
 
 # runSearchAnalysis(["trump"], 10)
+# print(geocode("somewhere"))
